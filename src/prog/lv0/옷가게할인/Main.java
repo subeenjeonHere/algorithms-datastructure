23500d9f47cfbf1aec8e5a551c785b69a0d78226
Subeen Jeon <145312273+subeenjeonHere@users.noreply.github.com>
2024-02-14 13:22:34 +0900

    Add

---
+++ src/prog/lv0/옷가게할인/Main.java
@@ -0,0 +1,19 @@
+package prog.lv0.옷가게할인;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 100010;
+        int answer = 0;
+        if (price >= 500000) {
+            answer = price - (int) (price * 0.2);
+        } else if (price >= 300000) {
+            answer = price - (int) (price * 0.1);
+        } else if (price >= 100000) {
+            answer = (int)(price - (price * 0.05));
+        } else {
+            answer = price;
+        }
+        System.out.println(answer);
+    }
+}
