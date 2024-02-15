c8f87a84d5921f600802e863a566300d58bb7f1a
Subeen Jeon <145312273+subeenjeonHere@users.noreply.github.com>
2024-02-15 12:22:26 +0900

    add

--- src/demo/queue/Main.java
+++ src/demo/queue/Main.java
@@ -1,7 +1,28 @@
 package demo.queue;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Main {
     public static void main(String[] args) {
+
+        //Declare a Queue
+        Queue<Integer> que = new LinkedList<>();
+        //Initialize the queue with values
+
+        for (int i = 0; i < 10; i++) {
+            que.add(i);
+        }
+        System.out.println(que);
+
+        // traverse the Queue using Iterator
+        Iterator iterator = que.iterator();
+        while (iterator.hasNext()) {
+            String element = (String) iterator.next();
+            System.out.println(element);
+        }
+
 
     }
 }
