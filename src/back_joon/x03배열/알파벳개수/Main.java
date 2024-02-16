bf6ba523a228cd325cf361d600c0f4ce408fc376
Subeen Jeon <145312273+subeenjeonHere@users.noreply.github.com>
2024-02-16 18:39:27 +0900

    add

--- src/back_joon/x03배열/알파벳개수/Main.java
+++ src/back_joon/x03배열/알파벳개수/Main.java
@@ -1,39 +1,44 @@
 package back_joon.x03배열.알파벳개수;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 import java.util.logging.XMLFormatter;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
         char[] arr = new char[26];
+        int[] counting = new int[26];
+
+        Scanner sc = new Scanner(System.in);
         StringBuilder sb = new StringBuilder();
+        String S = sc.next();
+
 
         // a-z 저장할 배열
         int a = 97;
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = (char) a;
-            a++;
-        }
-        // String 입력
-        Scanner sc = new Scanner(System.in);
-        String S = sc.next();
-
-        // arr 길이까지 반복, Str.charAt(i)와 arr[i]가 같은 경우 a-z의 개수를 카운팅 할 temp 배열을 1씩 카운팅
-        int[] counting = new int[26];
-        for (int i = 0; i < S.length(); i++) {
-            for (int k = 0; k < arr.length; k++) {
-                if (S.charAt(i) == arr[k]) {
-                    counting[k]++;
+        int idx = 0;
+        while (a <= 122) {
+            for (int i = 0; i < S.charAt(i); i++) {
+                if ((char) a == S.charAt(i)) {
+                    counting[idx]++;
+                    idx++;
                 }
             }
+            a++;
         }
 
         int start = 0;
-        while (start < arr.length) {
+        while (start < counting.length) {
             sb.append(counting[start]).append(" ");
             start++;
         }
         System.out.println(sb);
+
+
     }
 }
 
