db1b7f5b8fa1ed2f00d0241d4cad11fabe3e5929
Subeen Jeon <145312273+subeenjeonHere@users.noreply.github.com>
2024-02-20 21:12:02 +0900

    add

--- src/prog/lv1/평균구하기/Main.java
+++ src/prog/lv1/평균구하기/Main.java
@@ -6,7 +6,9 @@ public class Main {
     public static void main(String[] args) {
 
         int[] arr = {1, 2, 3, 4};
+
         List<Integer> list = new ArrayList<>();
+
         for (int i = 0; i < arr.length; i++) {
             list.add(arr[i]);
         }
