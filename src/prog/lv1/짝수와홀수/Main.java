492922b1d51d86e9cae09a9ffadf424cf9de1314
Subeen Jeon <145312273+subeenjeonHere@users.noreply.github.com>
2024-02-21 09:27:58 +0900

    add

---
+++ src/prog/lv1/짝수와홀수/Main.java
@@ -0,0 +1,12 @@
+package prog.lv1.짝수와홀수;
+
+public class Main {
+    public static void main(String[] args) {
+        int num = 3;
+
+        String answer = (num % 2 == 0) ? "Even" : "Odd";
+        System.out.println(answer);
+
+
+    }
+}
