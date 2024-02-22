047bd38c6c131bf20bb1a85106f2cb93288cba35
Subeen Jeon <145312273+subeenjeonHere@users.noreply.github.com>
2024-02-22 09:26:10 +0900

    add

--- src/baek_joon/x03배열/두수의합/Main.java
+++ src/baek_joon/x03배열/두수의합/Main.java
@@ -1,10 +1,14 @@
 package baek_joon.x03배열.두수의합;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.SequenceInputStream;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         //배열 생성
@@ -20,22 +24,16 @@ public class Main {
 
         int cnt = 0;
         int start = 0;
-        int end = 1;
-
-        // a + b = x인 순서쌍 구하기, a < b
-        // start가 arr 길이까지 올 떄
-        while (end < arr.length) {
+        int end = arr.length - 1;
+        while (start < end) {
             if (arr[start] + arr[end] == x) {
-                System.out.println(arr[start] + "  " + arr[end]);
                 cnt++;
-                start++;
-                // 같은 후, end 복귀해줘야 함
-                end = start + 1;
+                end--;
             } else if (arr[start] + arr[end] < x) {
-                end++;
-            } else {
                 start++;
-                end = start + 1;
+            } else if (arr[start] + arr[end] > x) {
+                System.out.println("Start:" + start + "// End:" + end);
+                end--;
             }
         }
         System.out.println(cnt);
