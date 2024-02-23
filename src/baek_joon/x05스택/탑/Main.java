0f1142bea896bd95cdbbd8dfab91ce6b3f15d6f9
Subeen Jeon <145312273+subeenjeonHere@users.noreply.github.com>
2024-02-24 00:12:16 +0900

    add

--- src/baek_joon/x05스택/탑/Main.java
+++ src/baek_joon/x05스택/탑/Main.java
@@ -1,53 +1,74 @@
 package baek_joon.x05스택.탑;
 
-import java.util.SortedMap;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
 import java.util.Stack;
 
 public class Main {
-    public static void main(String[] args) {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) throws IOException {
+
         Stack<Integer> stack = new Stack<>();
+
         StringBuilder sb = new StringBuilder();
-        int[] arr = {6, 9, 5, 7, 4};
+        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        int n = Integer.parseInt(br.readLine());
         //스택에 push
-        for (int i = 0; i < arr.length; i++) {
-            stack.push(arr[i]);
+        String[] tokens = br.readLine().split(" ");
+        for (String token : tokens) {
+            if (!token.trim().isEmpty()) {
+                stack.push(Integer.parseInt(token.trim()));
+            }
         }
-        System.out.println("스택: " + stack);
 
-        //스택 길이 만큼 순회
-        for (int j = 0; j <= stack.size(); j++) {
 
-            //Top 원소 peek
-            int top = stack.peek();
-            System.out.println("Now top: " + top);
-
-            //Top 원소 위치
-            int topIdx = stack.indexOf(top);
-            int start = topIdx;
-            System.out.println("Now Top Idx: " + topIdx);
-
-            while (start >= 0) {
-                System.out.println("[Start]: " + start);
-                if (top < stack.get(start)) {
-                    int light = stack.get(start);
-                    int lightIdx = stack.indexOf(light) + 1;
-                    //발사 가능한 탑 idx
-                    System.out.println("[발사탑] :" + stack.get(start) + "//발사탑 위치: " + lightIdx);
-
-                    //pop
-                    stack.pop();
-                    sb.append(lightIdx).append(" ");
-                    break;
-                }
-                if (top > stack.get(start)) {
-                    sb.append("0").append(" ");
-                }
-                start--;
+        while (!stack.isEmpty()) {
+            //스택 비어 있으면 종료
+            if (stack.isEmpty()) {
+                break;
             }
 
+            //스택 peek
+//            System.out.println("Next Peek");
+            int peek = stack.peek();
+//            System.out.println("[지금 Top]: " + peek);
 
+//            System.out.println("사이즈: " + stack.size());
+//            System.out.println();
+
+            //top을 기준으로 순회하는 for문
+            for (int j = stack.size() - 1; j >= 0; j--) {
+//                System.out.println("Stack 길이: " + stack.size() + " // 탐색위치: " + j + "// 탐색원소:" + stack.get(j));
+
+                if (peek < stack.get(j)) {
+//                    System.out.println("Bigger than peeked!");
+
+                    int idx = stack.indexOf(stack.get(j)) + 1;
+                    stack.pop();
+
+//                    System.out.println("최대값을 찾아 Pop한 후의 Top: " + stack.peek());
+//                    System.out.println();
+
+                    sb.append(idx).append(" ");
+
+                    //최대값 찾으면 I기준으로 탐색하는 For문 탈출
+                    break;
+                }
+            }
+            if (peek == stack.peek()) {
+//                System.out.println("최대값을 못 찾음   " + stack.peek());
+                stack.pop();
+                sb.append("0").append(" ");
+            }
         }
-        System.out.println(sb.reverse());
+        System.out.println(sb.reverse().toString().trim());
+        br.close();
     }
 }
