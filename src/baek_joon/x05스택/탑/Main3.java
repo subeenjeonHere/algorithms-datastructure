b6995bf30981bef57dc7ded5aaf9217dfd725a81
Subeen Jeon <145312273+subeenjeonHere@users.noreply.github.com>
2024-02-26 16:42:54 +0900

    add

--- src/baek_joon/x05스택/탑/Main3.java
+++ src/baek_joon/x05스택/탑/Main3.java
@@ -8,44 +8,102 @@ import java.util.Collections;
 import java.util.Stack;
 
 public class Main3 {
+    static class Tower {
+        int idx;
+        int height;
+
+        public Tower(int idx, int height) {
+            this.idx = idx;
+            this.height = height;
+        }
+    }
+
     public static void main(String[] args) throws IOException {
 
         Stack<Integer> stack = new Stack<>();
         StringBuilder sb = new StringBuilder();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
 
-        // n 입력받고 스택에 push
+        // n 입력 받고 스택에 push
         String[] tokens = br.readLine().split(" ");
+        System.out.println(Arrays.toString(tokens));
 
-        for (String token : tokens) {
-            if (!token.trim().isEmpty()) {
-                int ele = Integer.parseInt(token.trim());
-                stack.push(ele);
-            }
+        // 첫 번째 원소 Push
+        for (int i = 0; i < 1; i++) {
+            int first = Integer.parseInt(tokens[0].trim());
+            stack.push(first);
+        }
+
+        //Stack에 Push or Not
+        for (int i = 1; i < tokens.length; i++) {
+            System.out.println("Token길이: " + tokens.length + " //" + i);
 
             int peek = stack.peek();
-            int peekIdx = stack.size();
-            System.out.println("// Peek:" + peek + "//Peek Idx" + peekIdx);
+            int peekIdx = stack.indexOf(peek);
+            System.out.println("Top: " + peek);
 
-            int pt = peekIdx - 1;
-            while (pt >= 0) {
-                int temp = stack.elementAt(pt);
-                if (temp > peek) {
-                    System.out.println(stack.indexOf(temp));
-                    System.out.println(temp + " 큰 값 발견: " + stack.indexOf(pt) + "/// " + stack.peek());
+            //다음 토큰
+            int next = Integer.parseInt(tokens[i].trim());
+            System.out.println("Next: " + next);
 
-                    sb.append(pt + 1).append(" ");
-                    break;
-                }
+            //다음 토큰이 top 보다 크다면, 현쟈 top pop, "0" 입력, next 토큰 push
+            //다음 토큰이 작다면, 현재 top idx를 기록하고 push 하지 않음 > 작다면 뒤의 토큰들도 max인 현재 top에 발사할 것이기 때문
+            if (next > peek) {
+
+                stack.pop();
                 sb.append("0").append(" ");
+                stack.push(next);
 
-                pt--;
+                System.out.println("결과: : " + sb.toString());
+                System.out.println();
+
+            } else if (peek > next) {
+
+                sb.append(peekIdx).append(" ");
+
+                System.out.println("Bigger");
+                System.out.println("Top: " + peek + "// Next: " + next);
+                sb.append(peekIdx).append(" ");
+                System.out.println();
+
+                System.out.println("결과: : " + sb.toString());
+                System.out.println();
+
+                stack.push(next);
+                System.out.println();
+
             }
-
         }
+
+
+//        for (String token : tokens) {
+//            if (!token.trim().isEmpty()) {
+//                int ele = Integer.parseInt(token.trim());
+//                stack.push(ele);
+//            }
+//
+//            int peek = stack.peek();
+//            int peekIdx = stack.size();
+//            System.out.println("// Peek:" + peek + "//Peek Idx" + peekIdx);
+//
+//            int pt = peekIdx - 1;
+//            while (pt >= 0) {
+//                int temp = stack.elementAt(pt);
+//                if (temp > peek) {
+//                    System.out.println(stack.indexOf(temp));
+//                    System.out.println(temp + " 큰 값 발견: " + stack.indexOf(pt) + "/// " + stack.peek());
+//
+//                    sb.append(pt + 1).append(" ");
+//                    break;
+//                }
+////                sb.append("0").append(" ");
+//
+//                pt--;
+//            }
+//
+//        }
 
         System.out.println(sb.toString());
 
--- src/prog/lv2/Jaden문자열/Main.java
+++ src/prog/lv2/Jaden문자열/Main.java
@@ -5,27 +5,34 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-//        String s = "3people unFollowed me";
-        String s = "a    b";
+        String s = "3people unFollowed me";
+//        String s = "a    b";
         StringBuilder sb = new StringBuilder();
         String[] strings = s.split(" ");
         System.out.println(Arrays.toString(strings));
 
         for (int i = 0; i < strings.length; i++) {
             char myString = strings[i].charAt(0);
-            System.out.println(strings[i].charAt(0));
+
+            if (Character.isWhitespace(myString)) {
+                return;
+            }
             // 스페이스 여러 개인 경우 해결
+
 
             if (Character.isLowerCase(myString)) {
                 String a = String.valueOf(myString).toUpperCase();
                 String st = strings[i].substring(1, strings[i].length()).toLowerCase();
                 sb.append(a).append(st).append(" ");
+
             } else if ((int) myString >= 48 && (int) myString <= 57) {
                 String nums = String.valueOf(strings[i].charAt(1)).toLowerCase();
 
 
                 String strings3 = strings[i].substring(1, strings[i].length()).toLowerCase();
                 sb.append(myString).append(strings3).append(" ");
+            } else if (Character.isWhitespace(myString)) {
+                return;
             }
         }
         s = sb.toString().trim();
