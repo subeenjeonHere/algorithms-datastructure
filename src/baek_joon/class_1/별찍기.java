5e6317e14dd5b2a09d99721e0006c8ca3c9fa73a
Subeen Jeon <145312273+subeenjeonHere@users.noreply.github.com>
2024-03-01 21:21:52 +0900

    add

--- src/baek_joon/class_1/별찍기.java
+++ src/baek_joon/class_1/별찍기.java
@@ -12,11 +12,11 @@ public class 별찍기 {
         int n = sc.nextInt();
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print("*");
-//                sb.append("*");
+//                System.out.print("*");
+                sb.append("*");
             }
             System.out.println();
-//            sb.delete(0, sb.length());
+            sb.delete(0, sb.length());
         }
     }
 }
--- src/baek_joon/class_2/부녀회장이될테야.java
+++ src/baek_joon/class_2/부녀회장이될테야.java
@@ -32,15 +32,18 @@ public class 부녀회장이될테야 {
             //3층이면 1,2,3 누적합을 구해와야 함
             for (int st = 1; st <= k; st++) {
                 s3array = new int[array.length];
-                //누적합의 0번은 원본 0번째와 항상 동일하므로 초기화 해줘야 함
+
+                //누적합의 0번은 원본 0번째와 항상 동일하므로 초기화
                 s3array[0] = array[0];
                 //1층 누적합
                 for (int i = 1; i < 14; i++) {
                     s3array[i] = s3array[i - 1] + array[i];
                 }
                 array = s3array;
+
+                System.out.println(Arrays.toString(s3array));
+                sb.append(s3array[n - 1]).append("\n");
             }
-            sb.append(s3array[n - 1]).append("\n");
         }
         System.out.println(sb.toString().trim());
     }
