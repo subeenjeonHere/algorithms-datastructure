63f7a63c851f4d67e41b0cbe86871f3e785a2378
Subeen Jeon <145312273+subeenjeonHere@users.noreply.github.com>
2024-03-09 14:19:24 +0900

    Add: 나이순정렬

--- src/baek_joon/class_2/나이순정렬/Main.java
+++ src/baek_joon/class_2/나이순정렬/Main.java
@@ -1,75 +1,71 @@
 package baek_joon.class_2.나이순정렬;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Objects;
-import java.util.Scanner;
+import java.sql.Array;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-        String[][] arr = new String[3][2];
+        String[][] arr = new String[n][2];
 
-
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
+        int rows = arr.length;
+        int cols = arr[0].length;
+        //배열 저장
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
                 arr[i][j] = String.valueOf(sc.next());
             }
         }
+        /**
+         * @Note 2차원 배열 ArrayList에 저장
+         */
+        ArrayList<ArrayList<String>> arrayList = new ArrayList<>();
+        for (String[] row : arr) {
+            //각 행을 나타내는 ArrayList<String>을 데이터 추가
+            ArrayList<String> rowData = new ArrayList<>(Arrays.asList(row));
+            arrayList.add(rowData);
+        }
+        // 추가: 가입일 Idx
+        for (int i = 0; i < arrayList.size(); i++) {
+            arrayList.get(i).add(String.valueOf(i));
+        }
 
-        int min = Integer.parseInt(arr[0][0]);
+        // 정렬 나이순, 나이 같으면 가입순
+        // 나이가 같다면, idx를 보고 판단
+        for (int i = 0; i < arrayList.size(); i++) {
+            for (int j = i + 1; j < arrayList.size(); j++) {
+                int age1 = Integer.parseInt(arrayList.get(i).get(0));
+                int age2 = Integer.parseInt(arrayList.get(j).get(0));
+                int idx1 = Integer.parseInt(arrayList.get(i).get(2));
+                int idx2 = Integer.parseInt(arrayList.get(j).get(2));
 
-        //2차원 배열 선택 정렬
-        String temp;
-        for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = i + 1; j < arr.length; j++) {
+//                System.out.println("get(i): " + arrayList.get(i));
+//                System.out.println("get(2): " + arrayList.get(i).get(2));
 
-                if (arr[i][0].equals(arr[j][0])) {
-                    System.out.println("나이 같음");
-                    System.out.println(" arr[i][0]: " + arr[i][0]);
-                    System.out.println(" arr[j][0]: " + arr[j][0]);
+                //나이 순 오름차순 정렬
+                if ((age1 == age2 && idx1 > idx2) || age1 > age2) {
+                    //Swap
+//                    ArrayList<String> temp = new ArrayList<>(arrayList.get(i));
+//                    arrayList.set(i, new ArrayList<>(arrayList.get(j)));
+//                    arrayList.set(j, new ArrayList<>(temp));
+                    Collections.swap(arrayList, i, j);
                 }
-                if (arr[i][0].compareTo(arr[j][0]) > 0 && !arr[i][0].equals(arr[j][0])) {
-                    System.out.println(i + " I : J " + j);
-                    /**
-                     * 나이 같으면 먼저 가입한 사람 앞에 옴
-                     */
-                    for (int k = 0; k < arr[i].length; k++) {
-                        System.out.println(" arr[i][k]: " + arr[i][k]);
-                        System.out.println("arr[j][k]: " + arr[j][k]);
-                        temp = arr[i][k];
-                        System.out.println("Temp: " + temp);
-                        arr[i][k] = arr[j][k];
-                        arr[j][k] = temp;
-                    }
-                }
-
-//                int temp = 0;
-//                System.out.println("arr[j][0]: " + arr[j][0]);
-//                if (min > Integer.parseInt(arr[j][0])) {
-//                    //[j][0] = 20
-//                    // Now min = 21
-//                    System.out.println("Min" + min);
-//                    // 21 20 , 20을 temp, 21을 20자리, temp의 20을 21 자리
-//                    temp = Integer.parseInt(arr[j][0]);
-//                    arr[j][0] = String.valueOf(min);
-//                    min = temp;
-//
-//
-//                }
             }
         }
 
-        for (int p = 0; p < arr.length; p++) {
-            for (int q = 0; q < arr[p].length; q++) {
-                System.out.print(arr[p][q] + " ");
-            }
-            System.out.println();
+        for (ArrayList<String> row : arrayList) {
+            row.remove(2);
         }
+        StringBuilder sb = new StringBuilder();
+        for (int p = 0; p < arrayList.size(); p++) {
+            for (int q = 0; q < arrayList.get(p).size(); q++) {
+                sb.append(arrayList.get(p).get(q)).append(" ");
+            }
 
-
-        System.out.println(Arrays.deepToString(arr));
+            sb.append("\n");
+        }
+        System.out.print(sb.toString().trim());
     }
 }
