ea1361b67e8cdc5ff5aee7089c9699d7232fa28a
Subeen Jeon <145312273+subeenjeonHere@users.noreply.github.com>
2024-03-16 09:37:46 +0900

    Close #20

--- src/baek_joon/class_3/유기농배추/Main_BFS.java
+++ src/baek_joon/class_3/유기농배추/Main_BFS.java
@@ -9,17 +9,21 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 /**
- * @Subject BFS 풀이
+ * @Subject - BFS 풀이
  */
 
 /**
  * @Point 왜 그래프 n+2, m+2로 해줘야 하는지
+ * 큐 BFS 4방탐색
+ * 예제 2에서 4,0 탐색 안 됨
+ * 4방탐색 continue 조건 설정
  */
 public class Main_BFS {
     // 가로 m, 세로 n, 노드 k
     static int m, n, k;
     static int[][] graph;
     static boolean[][] visit;
+    static int count;
 
     public static void main(String[] args) throws IOException {
 
@@ -36,101 +40,65 @@ public class Main_BFS {
             k = Integer.parseInt(info[2]);
 
             // 인접 행렬 할당
-            graph = new int[n + 2][m + 2];
-            visit = new boolean[n + 2][m + 2];
-            for (int p = 0; p < graph.length; p++) {
-                for (int q = 0; q < graph[p].length; q++) {
-                    String[] ab = br.readLine().split(" ");
-                    int x = Integer.parseInt(ab[0]);
-                    int y = Integer.parseInt(ab[1]);
-                    graph[x][y] = 1;
-                }
+            graph = new int[51][51];
+            visit = new boolean[51][51];
+
+            //인접 행렬 할당 2
+            for (int j = 1; j <= k; j++) {
+                String[] ab = br.readLine().split(" ");
+                int x = Integer.parseInt(ab[0]);
+                int y = Integer.parseInt(ab[1]);
+                graph[x][y] = 1;
             }
             for (int x = 0; x < graph.length; x++) {
-                for (int y = 0; y < graph[y].length; y++) {
+                for (int y = 0; y < graph[x].length; y++) {
                     if (graph[x][y] == 1 && !visit[x][y]) {
+                        count++;
                         bfs(x, y, visit, graph);
                     }
                 }
             }
-
+            System.out.println(count);
+            count = 0;
         }
-
     }
 
     //BFS 시작
     private static void bfs(int x, int y, boolean[][] visit, int[][] graph) {
+
         //델타 4방 탐색
         int[] dx = {-1, 1, 0, 0};
         int[] dy = {0, 0, -1, 1};
-        int[][] dir = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-
-        Queue<Integer> que = new LinkedList<>();
-
-        //큐 선언
-        Queue<pair> queue = new LinkedList<>();
-
-        //큐에 시작노드 삽입
-        que.add(graph[x][y]);
-
-        //시작노드 삽입
-        queue.add(new pair(x, y));
+        //큐 배열 선언
+        Queue<int[]> queue = new LinkedList<>();
+        //큐에 시작노드 좌표 삽입
+        queue.add(new int[]{x, y});
 
         //방문 체크
         visit[x][y] = true;
+        //BFS 반복
+        while (!queue.isEmpty()) {
 
-        // 큐 빌 때까지 반복
-        while (queue.size() > 0) {
-            pair p = queue.peek();
-            queue.remove();
+            //좌표배열 있어야 함
+            int[] current = queue.poll();
+            int nowX = current[0];
+            int nowY = current[1];
 
-            // 방문체크
-            visit[p.node1][p.node2] = false;
-
-            //4방탐색
+            //4방 탐색 인접 행렬
             for (int d = 0; d < 4; d++) {
-                //4방 탐색 좌표
-                int a = p.node1 + dir[d][0];
-                int b = p.node2 + dir[d][1];
+                int nx = nowX + dx[d];
+                int ny = nowY + dy[d];
 
-                if (a >= 0 && b >= 0 && a < n && b < m && !visit[a][b]) {
-                    queue.add(new pair(a, b));
+                if (nx < 0 || ny < 0 || nx >= graph.length || ny >= graph[0].length) {
+                    continue;
                 }
-
-
+                // 큐에 방문하지 않았고, 4방 탐색 좌표가 1이라면
+                if (graph[nx][ny] == 1 && !visit[nx][ny]) {
+                    //방문 처리
+                    visit[nx][ny] = true;
+                    queue.add(new int[]{nx, ny});
+                }
             }
-
-        }
-
-        while (!que.isEmpty()) {
-            //현재 노드에서 연결된 노드 4방 탐색
-            for (int d = 0; d < 4; d++) {
-                int nx = x + dx[d];
-                int ny = y + dy[d];
-
-                if (nx < 0 || ny < 0)
-                    if (graph[nx][ny] == 1 && !visit[nx][ny]) {
-                        visit[nx][ny] = true;
-                        que.add(graph[nx][ny]);
-                    }
-            }
-
-        }
-
-        //4방 탐색
-        //큐에 시작노드 삽입
-        //노드 방문 체크
-        //반복
-
-    }
-
-
-    static class pair {
-        int node1, node2;
-
-        pair(int f, int s) {
-            node1 = f;
-            node2 = s;
         }
     }
 }
