241b4e35424d38734e9e8170a95b2d0a71e84dac
Subeen Jeon <145312273+subeenjeonHere@users.noreply.github.com>
2024-03-17 12:46:43 +0900

    Close #22

--- src/baek_joon/class_3/미로탐색/Main2.java
+++ src/baek_joon/class_3/미로탐색/Main2.java
@@ -3,24 +3,30 @@ package baek_joon.class_3.미로탐색;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
+
 
 /***
  * @Point
- *
- * (0, 3) -> (0,4) 탐색못하고 continue됨 = nx,ny 길이
- * 인접노드 여러개일 때 한개의 최단경로를 찾아내야 함
+
+ * 인접노드 여러개일 때 한 개의 최단경로
  */
 public class Main2 {
     //방문 배열
     static boolean[][] visit;
-    //인접행렬 배열
+    //인접 행렬 배열
     static int[][] g;
     //노드 탐색 카운트 int
     static int count;
-    //세로,가로길이
+    //세로,가로 길이
     static int n, m;
+    //경로 배열
+    static int[] path;
+    static ArrayList<Integer>[] path2;
+    //거리 배열
+    static int[] dis;
+    static ArrayList<int[]> distance;
+    static ArrayList<Integer>[] dis2;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -30,9 +36,14 @@ public class Main2 {
         n = Integer.parseInt(nm[0]);
         m = Integer.parseInt(nm[1]);
 
+        //인접행렬 메모리 할당
         g = new int[n][m];
         //방문 배열 할당
         visit = new boolean[n][m];
+        // 경로 배열 할당 노드 수
+        path = new int[n * m];
+        //거리 배열 할당
+        dis = new int[n * m];
 
         //인접행렬 입력받기
         for (int i = 0; i < n; i++) {
@@ -41,6 +52,7 @@ public class Main2 {
                 g[i][j] = Integer.parseInt(node[j]);
             }
         }
+        //출력
         System.out.println();
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
@@ -48,49 +60,117 @@ public class Main2 {
             }
             System.out.println();
         }
-        bfs(0, 0);
-        System.out.println(count);
+
+        //거리 배열 초기화
+        Arrays.fill(dis, -1);
+
+        //시작 정점 초기화
+        dis[g[0][0]] = 0;
+
+        //시작 노드 경로 초기화
+        path[g[0][0]] = g[0][0];
+
+//        bfs(0, 0);
+        System.out.println(bfs(0, 0));
+
     }
 
-    private static void bfs(int x, int y) {
-        //델타배열
+    private static int bfs(int x, int y) {
+        //델타 배열
         int[] dx = {-1, 1, 0, 0};
         int[] dy = {0, 0, -1, 1};
-        //bfs 큐로 구현
-        //큐 좌표 저장
-        //큐 자료구조 할당
+
+        //큐 할당
         Queue<int[]> que = new LinkedList<>();
-        //큐에 시작노드 삽입
+
+        Queue<Point> q = new LinkedList<>();
+
+        //큐에 시작 노드 삽입
         que.add(new int[]{x, y});
+        //큐 시작 위치, 거리 삽입
+        q.add(new Point(0, 0, 0));
+
         //방문 체크
         visit[x][y] = true;
-        //큐 빌 때 까지 반복
-        while (!que.isEmpty()) {
-            int[] nowNode = que.poll();
-            int curX = nowNode[0];
-            int curY = nowNode[1];
+        visit[0][0] = true;
+
+        while (!q.isEmpty()) {
+            Point now = q.poll();
+
+            int nowX = now.getX();
+            int nowY = now.getY();
+            int dis = now.getDis();
+            if (nowX == n - 1 && nowY == m - 1) {
+                return dis;
+            }
+
             for (int d = 0; d < 4; d++) {
-                int nx = curX + dx[d];
-                int ny = curY + dy[d];
+                int newX = nowX + dx[d];
+                int newY = nowY + dy[d];
 
-
-                if (nx < 0 || ny < 0 || nx >= n || ny >= m) {
+                if (newX < 0 || newY < 0 || newX >= n || newY >= m) {
                     continue;
                 }
-                if (g[nx][ny] == 1 && !visit[nx][ny]) {
-                    count++;
-                    visit[nx][ny] = true;
-                    que.add(new int[]{nx, ny});
+                // 인접 노드 1이고, 방문하지 않았다면 방문체크 큐 삽입 거리 +1 갱신
+                if (g[newX][newY] == 1 && !visit[newX][newY]) {
+                    visit[newX][newY] = true;
+                    q.add(new Point(newX, newY, dis + 1));
                 }
             }
         }
-        //큐에서 poll (제일 먼저 들어온 노드)
-        //4방 탐색
-        //큐랑 인접노드이고 방문하지 않았다면
-        //큐에 인접노드 삽입
-        //방문 체크
 
+        //큐 빌 때 까지 반복
+//        while (!que.isEmpty()) {
+//            int[] nowNode = que.poll();
+//            int curX = nowNode[0];
+//            int curY = nowNode[1];
+//            for (int d = 0; d < 4; d++) {
+//                int nx = curX + dx[d];
+//                int ny = curY + dy[d];
+//                if (nx < 0 || ny < 0 || nx >= n || ny >= m) {
+//                    continue;
+//                }
+//                //4방 탐색 인접 노드 찾기
+//                //인접한 노드의 거리가 -1 이라면 아직 방문하지 않았다
+//                //인접한 노드의 거리를 현재 노드 + 1 로 갱신해주어야 한다
+//                //현재 경로 정점을 인접 노드로 갱신
+//                //인접노드 큐 add
+//                if (g[nx][ny] == 1 && !visit[nx][ny]) {
+//                    count++;
+//                    visit[nx][ny] = true;
+//                    que.add(new int[]{nx, ny});
+//                }
+//            }
+//        }
+        return -1;
+    }
+
+    //데이터 쌍 저장할 Pair 클래스
+    public static class Point {
+        private final int x, y, dis;
+
+        public Point(int x, int y, int dis) {
+            this.x = x;
+            this.y = y;
+            this.dis = dis;
+        }
+
+        public int getX() {
+            return x;
+        }
+
+        public int getY() {
+            return y;
+        }
+
+        public int getDis() {
+            return dis;
+        }
     }
 }
 // 인접노드 여러 개 일 때 한 꺼번에 큐에 삽입?
 // 이걸로 어떻게 최단경로를 구할 수 있는건지
+// 인접행렬이 아니라 인접된 노드를 for 돌려야 하는건지 그럼 4방 탐색은 어떻게 할건지
+// (0, 3) -> (0,4) 탐색못하고 continue됨 = nx,ny 길이
+// bfS 종료 시점
+// 거리 출력 void -> int, 리턴값
