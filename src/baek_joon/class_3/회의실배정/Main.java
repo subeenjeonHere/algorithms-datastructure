36aeff3b0ad4bee44a8566564537ff0cefd99e32
Subeen Jeon <145312273+subeenjeonHere@users.noreply.github.com>
2024-03-21 10:33:43 +0700

    Fix 회의실배정

--- src/baek_joon/class_3/회의실배정/Main.java
+++ src/baek_joon/class_3/회의실배정/Main.java
@@ -1,10 +1,10 @@
 package baek_joon.class_3.회의실배정;
 
 import javax.crypto.Cipher;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 /**
  * @Point 그리디
@@ -24,7 +24,6 @@ public class Main {
     public static void main(String[] args) throws IOException {
         //N개 회의 입력 받기
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
         //회의 개수 입력받기
         int n = Integer.parseInt(br.readLine());
@@ -36,10 +35,8 @@ public class Main {
         //리스트에 행 할당, 회의 시간표 저장
         for (int i = 0; i < n; i++) {
             arrayList.add(new ArrayList<>());
-
             String[] info = br.readLine().split(" ");
             int st = Integer.parseInt(info[0]);
-
             arrayList.get(i).add(st);
             for (int j = 0; j < 1; j++) {
                 int end = Integer.parseInt(info[1]);
@@ -48,62 +45,66 @@ public class Main {
         }
 
         //전체 회의를 종료 시간 순으로 오름차순 정렬
-        int max = arrayList.get(0).get(1);
-        for (int i = 1; i < arrayList.size(); i++) {
-            int temp = arrayList.get(i).get(1);
-            if (max > temp) {
-                max = temp;
-            }
-        }
-        int max2 = arrayList.get(0).get(1);
-        int temp = 0;
-        for (int i = 0; i < arrayList.size(); i++) {
-            for (int j = i + 1; j < arrayList.size(); j++) {
-                temp = arrayList.get(j).get(1);
-
-                if (temp < max2) {
-
-
-                }
-
-
-            }
-        }
-
-
+        //종료시간이 같다면 시작 시간 빠른 것 우선
+        //종료시간이 같은 경우를 체크하기 위해 종료시간을 시작 시간 변수에 저장해두고 종료시간 같은 경우가 오면 비교
+//        int min = arrayList.get(0).get(1);
+//        int temp = 0;
+//        int s2 = 0;
+//        int e2 = 0;
+//        for (int i = 0; i < arrayList.size() - 1; i++) {
+//            e2 = arrayList.get(i + 1).get(1); //종료 시간
+//            s2 = arrayList.get(i + 1).get(0); //시작 시간
+//            //선택된 회의 종료 시간이 최소 종료시간 보다 작다면
+//            //해당 행 결과리스트에 저장
+//            if (min > e2) {
+//                resultList.add(arrayList.get(i + 1));
+//            }
+//        }
         //가장 첫 번째 종료 회의를 인덱스에 넣어줌
         resultList.add(arrayList.get(0));
 
-        int finalStart = arrayList.get(arrayList.size() - 1).get(0);
-
+        //종료 시간 같은 경우, 시작 시간 빠른 순으로 정렬
+        //Comparator 사용해야 함
+        Collections.sort(arrayList, new Comparator<ArrayList<Integer>>() {
+            @Override
+            public int compare(ArrayList<Integer> o1, ArrayList<Integer> o2) {
+                //종료 시간에 대해 비교
+                int endTimeCompare = o1.get(1).compareTo(o2.get(1));
+                //종료시간이 같다면, 시작 시간으로 비교
+                if (endTimeCompare == 0) {
+                    return o1.get(0).compareTo(o2.get(0));
+                }
+                //종료시간 다르면, 그 결과 반환
+                return endTimeCompare;
+            }
+        });
+        System.out.println(arrayList);
 
         //종료시간이 같은 경우, 시작 시간이 더 빠른 걸 선택하는 것이 유리함
         //if (i-1)회의 시작시간 < 종료시간 이라면 선택
-        for (int i = 0; i < arrayList.size(); i++) {
-
+        for (int i = 0; i < arrayList.size() - 1; i++) {
             //항상 선택된 회의의 맨 마지막 기준
             int len = resultList.size();
             int e1 = resultList.get(len - 1).get(1);
-
             for (int j = 1; j < arrayList.size(); j++) {
                 int s1 = arrayList.get(j).get(0);
                 if (s1 >= e1) {
+
                     resultList.add(arrayList.get(j));
+                    arrayList.remove(arrayList.get(j));
                     break;
                 }
-
-
-            }
-            //시간초과 어떻게 해결
-            //원래 시간표의 시작 시간보다 결과 배열의 종료시간이 더 커진다면 종료
-            int len2 = resultList.size();
-            int e2 = resultList.get(len2 - 1).get(1);
-
-            if (finalStart <= e2) {
-                break;
             }
         }
-        System.out.print(resultList.size());
+
+        for (int i = 1; i < arrayList.size(); i++) {
+            int s1 = arrayList.get(i - 1).get(1);
+
+        }
+        System.out.println(arrayList);
+        System.out.println("결과" + resultList);
+        System.out.println(resultList.size());
+
     }
 }
 
