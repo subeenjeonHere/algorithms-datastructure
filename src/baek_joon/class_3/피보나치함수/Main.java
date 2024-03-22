b47de16ab4f3a476ec053774af7f17f3537211a6
Subeen Jeon <145312273+subeenjeonHere@users.noreply.github.com>
2024-03-22 11:18:14 +0700

    Fix 피보나치함수

--- src/baek_joon/class_3/피보나치함수/Main.java
+++ src/baek_joon/class_3/피보나치함수/Main.java
@@ -6,76 +6,69 @@ import java.util.Scanner;
  * @Point - 피보나치 메모이제이션 0,1 호출될 때 어떻게 카운트 할 것인지
  */
 public class Main {
-    static int res0;
-    static int res1;
+
+    //메모이제이션 배열
     static int[] memo;
     static int[] count;
 
+    //카운팅
+    static int res_0;
+    static int res_1;
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
+        //테스트 케이스 개수
         int tc = sc.nextInt();
+
         for (int i = 1; i <= tc; i++) {
-            res0 = 0;
-            res1 = 0;
 
             int n = sc.nextInt();
             memo = new int[n + 1];
             count = new int[n + 1];
-            fibonacci(n);
-            System.out.println(res0 + " " + res1);
 
-//            if (n == 0) {
-//                System.out.println(count[0] + " " + "0");
-//            } else if (n == 1) {
-//                System.out.println("0" + " " + count[1]);
-//            } else {
-//                System.out.println(count[0] + " " + count[1]);
-//            }
+            //피보나치 0과 1은 무조건 0,1이므로 미리 할당
+            memo[0] = 0;
+            memo[1] = 1;
+
+            System.out.println();
+            System.out.println(fibonacci(n));
+            System.out.println(res_0 + " " + res_1);
 
         }
     }
 
+    //피보나치 구하기
+    //피보나치 재활용할 배열을 0으로 셋해두고, 만약에 0이 아니라면 값이 채워져있기에 재활용 할 수 있다는 것
+    //메모 배열에 피보나치 저장이 우선
+    //메모 배열이 != 0 이라면, 즉 재활용 할 수 있다면 해당 값 린턴
     private static int fibonacci(int n) {
-//        count[n] += 1;
-//        if (n == 0) {
-//            return 0;
-//        } else if (n == 1) {
-//            return 1;
-//        }
-//        return fibonacci(n - 1) + fibonacci(n - 2);
 
-        if (n <= 1) {
+        //0, 1 몇 번 호출됐는지 출력할 카운트 초기화
+        res_0 = 0;
+        res_1 = 0;
 
-            if (n == 0) {
-                res0 += 1;
-            } else if (n == 1) {
-                res1 += 1;
-            }
-
-            return n;
-        } else if (count[n] != 0) {
-            return count[n];
+        //0과 1이 오면, 메모 배열의 0,1을 리턴해줌
+        if (n == 0) {
+            res_0 += 1;
+            return memo[0];
+        } else if (n == 1) {
+            res_1 += 1;
+            return memo[1];
         }
-        return count[n] = fibonacci(n - 1) + fibonacci(n - 2);
 
-        // 메모이제이션 배열 초기화되지 않았다면
-//        if (n <= 1) {
-//            return n;
-//        } else if (memo[n] != 0) {
-//            return memo[n];
-//        } else
-//            return memo[n] = fibonacci(n - 1) + fibonacci(n - 2);
+        if (memo[n] == 0) {
+            memo[n] = fibonacci(n - 1) + fibonacci(n - 2);
+        } else if (memo[n] != 0) {
+            return memo[n];
+        }
 
-//        if (n == 0) {
-//            res0 += 1;
-//            return 0;
-//        } else if (n == 1) {
-//            res1 += 1;
-//            return 1;
-//        }
-//        return fibonacci(n - 1) + fibonacci(n - 2);
+        return fibonacci(n - 1) + (n - 2);
     }
 }
+//피보나치 3을 호출하면
+// 1. 2 + 1
+// 1.1. 1 + 0
+
+//여기서 1.1의 1을 재활용 할 수 있다.
