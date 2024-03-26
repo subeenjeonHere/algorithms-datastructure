c8d514e43627d6cd39c3fc1b58b602b97e54f7d4
Subeen Jeon <145312273+subeenjeonHere@users.noreply.github.com>
2024-03-26 19:35:52 +0900

    Close #4 랜선자르기

--- src/baek_joon/class_2/랜선자르기/Main.java
+++ src/baek_joon/class_2/랜선자르기/Main.java
@@ -3,112 +3,66 @@ package baek_joon.class_2.랜선자르기;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
+/**
+ * @Review 반복 범위
+ * <p>
+ * 웬만한 테케 통과하는데 몇몇 반례의 +1로 출력됨
+ */
 public class Main {
+    //k랜선 저장 배열
+    static int[] arr;
+    //k,n
+    static int k, n;
+    //랜선 개수 체크 카운팅 변수
+    static long count;
+    //최소, 최대값
+    static long min, max;
+    //중간값
+    static long mid;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stk = new StringTokenizer(br.readLine(), " ");
+        //K, N 입력받기
+        //갖고있는 랜선 K, 필요한 랜선 N
+        String[] kn = br.readLine().split(" ");
+        k = Integer.parseInt(kn[0]);
+        n = Integer.parseInt(kn[1]);
 
-        int k = Integer.parseInt(stk.nextToken());
-        int n = Integer.parseInt(stk.nextToken());
+        //K 줄에 갖고 있는 랜선 길이 입력
+        //배열 할당
+        arr = new int[k];
 
-        int[] arr = new int[k];
-        for (int i = 0; i < k; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-        }
-        // 최소값, 최대, 중간값
-        int max = arr[0];
-        int temp = 0;
+        min = 1;
+        max = Long.MAX_VALUE;
+
+
         for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
             if (max < arr[i]) {
-                temp = max;
                 max = arr[i];
-                arr[i] = temp;
             }
         }
-        long target = n;
-        long left = 1;
-        long right = max;
+        System.out.print(BS());
+    }
 
-        right++;
-/**
- * @Check Right 범위,
- */
-        while (left <= right) {
-            System.out.println("While 호출");
-
-            //Mid
-            long mid = left + (right - left) / 2;
-            System.out.println("Mid, Left, Right: " + mid + " " + left + "  " + right);
-            long sum = 0;
+    private static long BS() {
+        //중간값
+        mid = 0;
+        while (min <= max) {
+            //랜선개수
+            count = 0;
+            //중간값 찾기
+            mid = min + (max - min) / 2;
             for (int i = 0; i < arr.length; i++) {
-                sum += arr[i] / mid;
-                System.out.println("sum 1: " + sum);
+                count += arr[i] / mid;
             }
-            System.out.println("sum-" + sum);
-            if (sum < target) {
-                right = mid - 1;
-                System.out.println("right-" + right);
-            } else if (sum >= target) {
-                Math.max(mid, right);
-                left = mid + 1;
-                System.out.println("left- " + left);
+            if (count < n) {
+                max = mid - 1;
+            } else if (count >= n) {
+                min = mid + 1;
             }
         }
-
+        return Math.min(mid, min - 1);
     }
 }
-
-//            while (left <= right) {
-//            long middle = left + (right - left) / 2;
-//            int checkResult = check(arr, middle, target, left, right);
-//                System.out.println();
-//                System.out.println("결과1: " + checkResult);
-//
-//                if (checkResult == target) {
-//                    System.out.println(middle);
-//                    break;
-//                }
-//                //mid 길이로 만들 수 있는 랜선 수가 적다면 랜선을 줄여야 함
-//                else if (checkResult < target) {
-//                    right = middle - 1;
-//                    System.out.println();
-//                    System.out.println("길이와 Right: " + middle + " " + right);
-//                    /**
-//                     * @Check
-//                     * Middle이 right이 변경 된 이후 재계산하여 200이 다시 되어야 함
-//                     */
-//                    System.out.println("결과2: " + checkResult);
-//
-//                    //mid 길이로 만들 수 있는 랜선 수를 초과한다면, 랜선길이 늘려야 함
-//                } else if (checkResult > target) {
-//                    left = middle + 1;
-//                    System.out.println();
-//                    System.out.println("Mid, Left, Right: " + middle + " " + left + "  " + right);
-//
-//                    System.out.println("결과3: " + checkResult);
-//                }
-//            }
-//        }
-//    }
-
-
-//중간값으로 target개 랜선 만들어지는지 check
-//    private static int check(int[] arr, long mid, int target, long left, long right) {
-//        int count = 0;
-//        int st = 0;
-////        mid = left + (right - left) / 2;
-//        System.out.println("중간값 체크: " + mid);
-//
-//        while (st < arr.length) {
-//            count += arr[st] / mid;
-//            System.out.println("랜선 한 개당 만들 수 있는 : " + arr[st] / mid);
-//            st++;
-//        }
-//        System.out.println("만들 수 있는 랜선 수 : " + count);
-//        System.out.println();
-//        return count;
-//    }
-//    }
-
