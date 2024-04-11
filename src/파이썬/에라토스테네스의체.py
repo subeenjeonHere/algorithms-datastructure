9b673ce96b5aca20aba9829ea9da1cf7d6e59d7f
Subeen Jeon <145312273+subeenjeonHere@users.noreply.github.com>
2024-04-11 17:50:40 +0900

    Fix #51 에라토스테네스의체 python

--- src/파이썬/에라토스테네스의체.py
+++ src/파이썬/에라토스테네스의체.py
@@ -1,23 +1,11 @@
-# N보다 작거나 같은 모든 소수를 찾느낟
-# 2부터 N까지 모든 정수를 적는다
-# 지우지 않은 가장 작은 수 찾는다
-# P = 소수
-# P를 지우고, P의 배수를 순서대로 지운다
-# 즉 2부터 시작하되 2 4 6 8 .. N 까지 지움
-# 3이 가장 작은 수, 3 9  ...N 까지 지움
-# 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
-# 1 3 5 7 9 11 13 15
-# 1 2 3 5 9 11 13 15만 남게 된다
 n, k = map(int, input().split(" "))
 li = list()
 
 # 2부터 배열 생성
 for i in range(2, n + 1):
     li.append(i)
-# print(li)
 
 cnt = 0
-le = 0
 for i in range(len(li)):
     if li[i] == 0:
         continue
@@ -39,5 +27,4 @@ for i in range(len(li)):
             if cnt == k:
                 print(li[j])
             li[j] = 0
-
 # for loop 내에서 List 길이 줄어드므로 index 에러 발생함
