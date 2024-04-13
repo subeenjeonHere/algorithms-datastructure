8245c5fecc17f5cb72b3208f31a44af59c5529b8
Subeen Jeon <145312273+subeenjeonHere@users.noreply.github.com>
2024-04-13 14:28:28 +0900

    Close #47 맥주마시면서걸어가기

--- src/baek_joon/class_3/맥주마시면서걸어가기/Main.java
+++ src/baek_joon/class_3/맥주마시면서걸어가기/Main.java
@@ -17,11 +17,12 @@ public class Main {
     static ArrayList<Point> list;
     static boolean[] visited;
 
+    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         tc = Integer.parseInt(br.readLine());
+        sb = new StringBuilder();
 
         //인접행렬 입력받기
         for (int i = 0; i < tc; i++) {
@@ -37,12 +38,17 @@ public class Main {
                 int b = Integer.parseInt(node[1]);
                 list.add(new Point(a, b));
             }
-            bfs(list, visited, 0, 0);
-
+            int result = bfs(list, visited, 0, 0);
+            if (result == 1) {
+                sb.append("happy").append("\n");
+            } else {
+                sb.append("sad").append("\n");
+            }
         }
+        System.out.print(sb.toString().trim());
     }
 
-    private static void bfs(ArrayList<Point> list, boolean[] visited, int x, int y) {
+    private static int bfs(ArrayList<Point> list, boolean[] visited, int x, int y) {
 
         //락페 좌표
         int rX = list.get(list.size() - 1).getX();
@@ -51,32 +57,34 @@ public class Main {
         Queue<Point> que = new LinkedList<>();
 
         //시작 위치 삽입
-        que.add(new Point(list.get(x).x, list.get(y).y));
-        int dis = 0;
+        que.add(new Point(x, y));
+        int dist = 0;
 
         //x,y 행렬 방문 체크
         visited[0] = true;
-
         while (!que.isEmpty()) {
             Point nowNode = que.poll();
             int curX = nowNode.getX();
+            int nX = list.get(curX).x;
             int curY = nowNode.getY();
+            int nY = list.get(curY).y;
 
-            if (curX == rX && curY == rY) {
-                System.out.println("Happy");
+            if (nX == rX && nY == rY) {
+                return 1;
             }
             for (int s = 0; s < list.size(); s++) {
-                int x2 = list.get(s).x; // 다음 좌표 x
-                int y2 = list.get(s).y; // 다음 좌표 y
+                int x2 = list.get(s).x; // 다음 좌표 x 값
+                int y2 = list.get(s).y; // 다음 좌표 y 값
                 int temp = x2 + y2;
-                dis = Math.abs(curX - x2) + Math.abs(curY - y2);
-                if (dis <= 1000 && !visited[s]) {
+                dist = Math.abs(nX - x2) + Math.abs(nY - y2);
+//                dis = Math.abs(curX - x2) + Math.abs(curY - y2);
+                if (dist <= 1000 && !visited[s]) {
                     visited[s] = true;
-                    bfs(list, visited, s, s);
+                    que.add(new Point(s, s));
                 }
             }
         }
-        System.out.println("Sad");
+        return 0;
     }
 
 
